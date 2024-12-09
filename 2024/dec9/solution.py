88debab1a589f63fe3ecc9804b6e642985133dec
Ernst Vilsbæk <19546796+ErnstVS@users.noreply.github.com>
2024-12-10 00:37:14 +0100

    Update solution.py

--- 2024/dec9/solution.py
+++ 2024/dec9/solution.py
@@ -3,52 +3,71 @@ from textwrap import wrap
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 file = os.path.join(script_dir, "input09.txt")
-# file = os.path.join(script_dir, "test09.txt")
+file = os.path.join(script_dir, "test09.txt")
 
 
 with open(file, "r") as f:
     input = wrap(f.read(), 2)
 
-#   print(input)
+#print(input)
 
-result = ""
+result = []
+
+count = 0
 for idx, i in enumerate(input):
     if len(i) == 2:
-        result += (str(idx) * int(i[0])) + ("." * int(i[1]))
-        file = i[0]
-        free = i[1]
+        if int(i[0]) > 0:
+            #print(i[0])
+            for x in range(int(i[0])):
+                result.append(idx)
+        else:
+            print("file = Zero")
+        if int(i[1]) > 0:
+            for x in range(int(i[1])):
+                result.append(".")
+            #print(i[1])
+        else:
+            continue
+            #print("space = Zero")
+        #result +=  (str(idx) * int(i[0])) + ("." * int(i[1]))
+        #file = i[0]
+        #free = i[1]
     elif len(i) == 1:
-        result += str(idx) * int(i[0])
-        file = i[0]
-        free = None
+        
+        if int(i[0]) > 0:
+            #print(i[0])
+            for x in range(int(i[0])):
+                result.append(idx)
+        else:
+            continue
+         #  print("file = Zero")
+        
+        #result += str(idx) * int(i[0])
+        #file = i[0]
+        #free = None
+    count += 1
 
     # print(idx, "File: ", file, " Free: ", free)
 
 
-tempres = result[::-1].replace(".", "")
-temp1 = ""
+tempres1 = []
+for x1 in reversed(result):
+    if x1 != ".":
+        tempres1.append(x1)
+
+temp1 = []
 y = 0
 for x in result:
     if x != ".":
-        temp1 += x
+        temp1.append(x)
     else:
-        temp1 += tempres[y]
+        temp1.append(tempres1[y])
         y += 1
 
-temp1 = temp1[0 : len(tempres)]
+temp1 = temp1[0 : len(tempres1)]
 
 ans = 0
 for idxa, a in enumerate(temp1):
     ans += idxa * int(a)
 
 print(ans)
-
-
-# print(result)
-# print(tempres)
-# print(temp1)
-
-#    print(x)
-
-# print(input)
-# print(result)
